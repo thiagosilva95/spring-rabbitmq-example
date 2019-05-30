207ed1b65b6e33f3b778b1814512468b9d54b5e6
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-05-30 10:08:15 -0300

    Initial commit

---
+++ src/main/java/com/tdev/example/springrabbitmqexample/SpringRabbitmqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tdev.example.springrabbitmqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqExampleApplication.class, args);
+	}
+
+}
